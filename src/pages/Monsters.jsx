e178880fd05d69e06b05e38d6766297a33484808
PabloTei <pteijeirolopez@gmail.com>
2023-02-17 22:57:56 +0100

    hook useFetch and axios

--- src/pages/Monsters.jsx
+++ src/pages/Monsters.jsx
@@ -1,6 +1,8 @@
-
+import useFetch from "../hooks/useFetch";
 
 const Monsters = () => {
+    const [data] = useFetch(`https://botw-compendium.herokuapp.com/api/v2/category/monsters`);
+    console.log(data);
     return (
         <main>Monsters</main>
     )
