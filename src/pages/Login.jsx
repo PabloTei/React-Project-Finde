843300b75eb6d7a582b91ef54cd3e3580021170a
PabloTei <pteijeirolopez@gmail.com>
2023-02-18 16:32:33 +0100

    diseño login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,6 +9,7 @@ const Login = () => {
   const { login } = useContext(UserContext);
   return (
     <main className="login">
+      <img src='https://www.seekpng.com/png/full/185-1858014_legend-of-zelda-breath-of-the-wild-png.png' alt='fondo-zelda'/>
       <input type="text" placeholder="Username" ref={inputRef} />
       <button onClick={() => login(inputRef.current.value)}>Login</button>
     </main>
