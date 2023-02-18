d99fe61e442f5443911dd0b6801bf4494e1dc67b
PabloTei <pteijeirolopez@gmail.com>
2023-02-18 20:34:01 +0100

    grid monsters

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,8 +2,7 @@ const ProjectCard = ({ monster }) => {
   return (
     <figure>
       <img src={monster.image} alt={monster.name} />
-      <h2>{monster.name}</h2>
-      <p>{monster.description}</p>
+      <h2>{monster.name.toUpperCase()}</h2>
     </figure>
   );
 };
