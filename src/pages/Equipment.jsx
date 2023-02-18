a0980727730a9da4c6c414147275cc6aa8365dcc
PabloTei <pteijeirolopez@gmail.com>
2023-02-18 13:52:00 +0100

    hook useDebounce

--- src/pages/Equipment.jsx
+++ src/pages/Equipment.jsx
@@ -1,43 +1,35 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import ProjectCard from '../components/ProjectCard';
-import useFetch from '../hooks/useFetch';
-import axios from "axios"
 
+import ProjectCard from '../components/ProjectCard';
 
 const Equipment = () => {
-
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState([]);
 
   useEffect(() => {
-      axios.get("https://botw-compendium.herokuapp.com/api/v2/category/equipment").then((res) => {
+    axios
+      .get('https://botw-compendium.herokuapp.com/api/v2/category/equipment')
+      .then((res) => {
         setData(res.data.data);
-        setFilter(res.data.data)
+        setFilter(res.data.data);
       });
-    }, []);
-  
-const filterEquipment = (value) => {
-  const type = data.filter((object) => (object.name.includes(value) || object.description.includes(value)));
-  setFilter(type);
-}
+  }, []);
+
+  const filterEquipment = (value) => {
+    const type = data.filter(
+      (object) => object.name.includes(value) || object.description.includes(value),
+    );
+    setFilter(type);
+  };
 
   return (
     <main>
-      <button
-      onClick={() => filterEquipment("sword")}
-      >Swords</button>
-            <button
-      onClick={() => filterEquipment("axe")}
-      >Axes</button>
-            <button
-      onClick={() => filterEquipment("club")}
-      >Club</button>
-            <button
-      onClick={() => filterEquipment("spear")}
-      >Spears</button>
-            <button
-      onClick={() => filterEquipment("shield")}
-      >Shields</button>
+      <button onClick={() => filterEquipment('sword')}>Swords</button>
+      <button onClick={() => filterEquipment('axe')}>Axes</button>
+      <button onClick={() => filterEquipment('club')}>Club</button>
+      <button onClick={() => filterEquipment('spear')}>Spears</button>
+      <button onClick={() => filterEquipment('shield')}>Shields</button>
       {filter.map((monster) => (
         <ProjectCard key={monster.id} monster={monster} />
       ))}
--- src/pages/Monsters.jsx
+++ src/pages/Monsters.jsx
@@ -1,34 +1,37 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import ProjectCard from '../components/ProjectCard';
-import axios from "axios"
+import useDebounce from '../hooks/useDebounce';
 
+import ProjectCard from '../components/ProjectCard';
 
 const Monsters = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState([]);
+  const debounceValue = useDebounce(filter, 1000)
 
-    const [data, setData] = useState([]);
-    const [filter, setFilter] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://botw-compendium.herokuapp.com/api/v2/category/monsters')
+      .then((res) => {
+        setData(res.data.data);
+        setFilter(res.data.data);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get("https://botw-compendium.herokuapp.com/api/v2/category/monsters").then((res) => {
-          setData(res.data.data);
-          setFilter(res.data.data)
-        });
-      }, []);
-    
   const filterFunction = (value) => {
     const arrayFilter = data.filter((monster) => monster.name.includes(value));
     setFilter(arrayFilter);
-  }
+  };
 
   return (
     <main>
-        <input
+      <input
         type="text"
         onChange={(ev) => {
-            filterFunction(ev.target.value)
+          filterFunction(ev.target.value);
         }}
-        />
-      {filter.map((monster) => (
+      />
+      {debounceValue.map((monster) => (
         <ProjectCard key={monster.id} monster={monster} />
       ))}
     </main>
