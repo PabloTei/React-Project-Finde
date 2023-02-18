69b3a0b23d7084608cadecea00099a70a2fe9759
PabloTei <pteijeirolopez@gmail.com>
2023-02-18 13:37:55 +0100

    filtros equipment por botones

--- src/pages/Equipment.jsx
+++ src/pages/Equipment.jsx
@@ -1,14 +1,44 @@
+import { useEffect, useState } from 'react';
 import ProjectCard from '../components/ProjectCard';
 import useFetch from '../hooks/useFetch';
+import axios from "axios"
+
 
 const Equipment = () => {
-  const [data] = useFetch(
-    `https://botw-compendium.herokuapp.com/api/v2/category/equipment`,
-  );
+
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState([]);
+
+  useEffect(() => {
+      axios.get("https://botw-compendium.herokuapp.com/api/v2/category/equipment").then((res) => {
+        setData(res.data.data);
+        setFilter(res.data.data)
+      });
+    }, []);
+  
+const filterEquipment = (value) => {
+  const type = data.filter((object) => (object.name.includes(value) || object.description.includes(value)));
+  setFilter(type);
+}
 
   return (
     <main>
-      {data.map((monster) => (
+      <button
+      onClick={() => filterEquipment("sword")}
+      >Swords</button>
+            <button
+      onClick={() => filterEquipment("axe")}
+      >Axes</button>
+            <button
+      onClick={() => filterEquipment("club")}
+      >Club</button>
+            <button
+      onClick={() => filterEquipment("spear")}
+      >Spears</button>
+            <button
+      onClick={() => filterEquipment("shield")}
+      >Shields</button>
+      {filter.map((monster) => (
         <ProjectCard key={monster.id} monster={monster} />
       ))}
     </main>
